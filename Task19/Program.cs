1350122f52270af018924b000716a81e13a028b8
BlackStalker2008 <114511758+Black-Stalker2008@users.noreply.github.com>
2022-10-22 18:41:48 +0500

    Решение 19 задачи

---
+++ Task19/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+bool Palindrom(int number)
+{
+    return number / 10000 == number % 10 && 
+    number / 1000 % 10 == number % 100 / 10;
+}
+
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs(num);
+
+if (num <= 10000 || num > 99999)
+{
+    Console.WriteLine("Введённое число не является пятизначным");
+}
+else
+{
+    bool palindrom = Palindrom(num);
+    Console.WriteLine("Является ли оно палиндромом?");
+    Console.WriteLine(palindrom ? "Да" : "Нет");
+}
+
+
