24ef06a240bd5aa684f4a112b00815791b04dea3
BlackStalker2008 <114511758+Black-Stalker2008@users.noreply.github.com>
2022-11-19 17:41:30 +0500

    Решение 47 задачи

---
+++ Task47/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns]; 
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            double num = rnd.NextDouble() * (max - min) + min;
+            matrix[i, j] = Math.Round(num, 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 5}, ");
+            else Console.Write($"{matrix[i, j], 5}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+Console.Write("Введите количество строк массива: m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = CreateMatrixRndDouble(m, n, -10, 10);
+PrintMatrix(matrix);
