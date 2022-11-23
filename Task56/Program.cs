8dcaa0cb7643774507dac7eceee0628c652b0f01
BlackStalker2008 <114511758+Black-Stalker2008@users.noreply.github.com>
2022-11-23 23:56:22 +0500

    Решение 56 задачи

---
+++ Task56/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}, ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+void RowLowSum(int[,] matrix)
+{
+    int minSum = 0;
+    int indexMinRow = 0;
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        minSum += matrix[0, i];
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            sum += matrix[i, j];
+        if (sum < minSum)
+        {
+            minSum = sum;
+            indexMinRow = i;
+        }
+        sum = 0;
+    }
+    Console.Write($"{indexMinRow + 1} строка с наименьшей суммой элементов: {minSum}");
+}
+
+
+Console.Write("Введите количество строк массива: m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = CreateMatrixRndInt(m, n, -9, 9);
+PrintMatrix(matrix);
+RowLowSum(matrix);
+
