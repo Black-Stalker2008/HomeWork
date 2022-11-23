b371fca43ee85a319029d60a2f5abb809a7f1a5f
BlackStalker2008 <114511758+Black-Stalker2008@users.noreply.github.com>
2022-11-23 22:52:01 +0500

    Решение 54 задачи

---
+++ Task54/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}, ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+void MatrixSortedRows(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+      {
+        if (matrix[i, k] < matrix[i, k + 1])
+        {
+          int temp = matrix[i, k + 1];
+          matrix[i, k + 1] = matrix[i, k];
+          matrix[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+
+Console.Write("Введите количество строк массива: m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = CreateMatrixRndInt(m, n, 0, 9);
+PrintMatrix(matrix);
+MatrixSortedRows(matrix);
+Console.WriteLine("Двумерный массив с упорядоченными по убыванию элементами каждой строки");
+PrintMatrix(matrix);
