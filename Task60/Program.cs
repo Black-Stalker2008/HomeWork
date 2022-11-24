1814c195bb23148f8d6c3c3a82e82f2f9d044005
BlackStalker2008 <114511758+Black-Stalker2008@users.noreply.github.com>
2022-11-24 21:52:01 +0500

    Решение 60 задачи

---
+++ Task60/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив,
+//  добавляя индексы каждого элемента.
+// Например, задан массив размером 2 x 2 x 2.
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+int[,,] array3D = new int[2, 2, 2];
+CreateArray3DRandom(array3D, 1, 12); // Если больше 12 то при max значениях будет 3х значное число
+Console.Write("Трехмерный массив из неповторяющихся двузначных чисел с индексами каждого элемента: ");
+PrintArray3D(array3D);
+
+void PrintArray3D(int[,,] arr)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"{array3D[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+void CreateArray3DRandom(int[,,] arr, int min, int max)
+{
+    Random rnd = new Random();
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] += count;
+                count += rnd.Next(min, max);
+            }
+        }
+    }
+}
